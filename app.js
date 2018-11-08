6a896bcbd19b09673388b60eaef0a958653cf598
IM-NOT-PHP-PROGRAMMER <35314850+IM-NOT-PHP-PROGRAMMER@users.noreply.github.com>
2018-11-08 23:51:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-console.log('it works!');
-while (true) {}
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('it works!');
+}).listen(8080); 
